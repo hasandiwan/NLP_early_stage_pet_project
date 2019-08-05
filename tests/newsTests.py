8b5b1b53f5f8f795d6370d767feb854443504ea9
Hasan Diwan <hd1@jsc.d8u.us>
2019-08-05 11:22:59 -0700

    add pseudocode

--- tests/newsTests.py
+++ tests/newsTests.py
@@ -2,6 +2,14 @@ import unittest
 
 import main
 
+"""
+1. Rename your authors_and_feeds.py to main.py
+2. Create a parse_link function in your file with one argument
+3. Retrieve the link.
+4. Run it through BeautifulSoup, end with the visible text of the page
+5. Apply SpaCy/textblob's NER functionality and retrieve the people and places mentioned in the url in the desired format.
+"""
+
 class NewsTests(unittest.TestCase):
     def testValidNerKeys(self):
         response = main.parse_link('https://www.theguardian.com/world/2019/jul/31/hamza-bin-laden-son-of-osama-bin-laden-has-died-reports')
